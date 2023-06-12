e5a339dfc75eacd7f87c0554744dc31a30969ab0
KiraCheng6 <zsjunzheng@gmail.com>
2023-06-12 18:50:15 +0800

    Update board.mk

--- trunk/configs/boards/NEWIFI/board.mk
+++ trunk/configs/boards/NEWIFI/board.mk
@@ -1,8 +1,8 @@
 ##################################################################
 # Board PID # Board Name       # PRODUCT # Note
 ##################################################################
-# NEWIFI   # NEWIFI          # MT7621  #
+# NEWIFI-D1 # LENOVO NEWIFI D1  # MT7621  #
 ##################################################################
 
-CFLAGS += -DBOARD_NEWIFI
+CFLAGS += -DBOARD_NEWIFID1 -DVENDOR_LENOVO
 BOARD_NUM_USB_PORTS=2
