ec77c1237028d61ebc1a9a2eb7f8dff54dc98622
KiraCheng6 <zsjunzheng@gmail.com>
2023-06-12 18:49:44 +0800

    Update board.h
    mod

--- trunk/configs/boards/NEWIFI/board.h
+++ trunk/configs/boards/NEWIFI/board.h
@@ -1,11 +1,11 @@
 /* NEWIFI */
 
-#define BOARD_PID		"NEWIFI"
-#define BOARD_NAME		"NEWIFI"
-#define BOARD_DESC		"NEWIFI Wireless Router"
-#define BOARD_VENDOR_NAME	"Diting Technology"
+#define BOARD_PID		"NEWIFI-D1"
+#define BOARD_NAME		"NEWIFI-D1"
+#define BOARD_DESC		"Lenovo NEWIFI-D1 Wireless Router"
+#define BOARD_VENDOR_NAME	"Lenovo(Beijing) Limited"
 #define BOARD_VENDOR_URL	"http://www.newifi.com/"
-#define BOARD_MODEL_URL		"http://www.newifi.com/"
+#define BOARD_MODEL_URL		"http://www.newifi.com/product_newifi2_summary.shtml"
 #define BOARD_BOOT_TIME		30
 #define BOARD_FLASH_TIME	120
 
